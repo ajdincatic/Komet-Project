600cc9ef11274c1a2a026119d6a5f4a354234c53
ajdin <ajdin.catic@edu.fit.ba>
2021-01-25 20:46:57 +0100

    .

--- frontend-ReactJS/src/Components/MainLayout/Wrapper.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "../../Style/Wrapper.module.css";
-
-export const Wrapper = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>;
-};
--- frontend-ReactJS/src/axios.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "http://komet-intern.qsd.ba/api";
-axios.defaults.headers.common["Content-Type"] = "application/json";
